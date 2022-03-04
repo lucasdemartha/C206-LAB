64978caa026a9f60473209bab9a34ebb8a6ccff2
Lucas Ribeiro de Martha <99219198+lucasdemartha@users.noreply.github.com>
2022-03-04 12:22:53 -0300

    Add files via upload
    Exercicio 1 - aula 4 - arrays

---
+++ Exercicio 1 - aula 4 - arrays/src/main/java/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno {
+
+    String nome;
+    int matricula, idade;
+
+    Aluno (String nome, int matricula, int idade){
+        this.nome = nome;
+        this.matricula = matricula;
+        this.idade = idade;
+    }
+}
---
+++ Exercicio 1 - aula 4 - arrays/src/main/java/Curso.java
@@ -0,0 +1,13 @@
+public class Curso {
+
+    String nome, tipo, turno;
+
+    Disciplina[] disciplina = new Disciplina[9];
+
+    Curso (String nome, String tipo, String turno){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.turno = turno;
+    }
+
+}
